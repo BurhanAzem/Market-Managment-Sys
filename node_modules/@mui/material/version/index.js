901a27d87c7d1e5dc577fe2f4d43152072749eb3
Burhan Azem <burhan12sab@gmail.com>
2025-02-22 16:37:34 +0200

    update/upgrade to toolkit && to.NET 9.0

--- node_modules/@mui/material/version/index.js
+++
@@ -1,6 +0,0 @@
-export const version = "6.4.5";
-export const major = Number("6");
-export const minor = Number("4");
-export const patch = Number("5");
-export const prerelease = undefined;
-export default version;
